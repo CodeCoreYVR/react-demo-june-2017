8d2740ccd71587bf6d45a56f9dbcfc660a576797
Steve Godin <steve.god@gmail.com>
2017-08-09 11:51:35 -0700

    Add Shape and OrangeBox components

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import Hello from './Hello';
+import Shape from './Shape';
+import OrangeBox from './OrangeBox';
 
 function App (props) {
+  const style = {
+    display: 'flex',
+    alignItems: 'center',
+    flexDirection: 'column'
+  };
+
   return (
-    <div className='App'>
+    <div className='App' style={style}>
       <Hello name='Jon' bgColor='LightCyan' />
       <Hello name='Daenerys' fontFamily='Papyrus' />
       <Hello name='Cersei' />
+      <OrangeBox />
+      <Shape />
+      <Shape bgColor='Red' />
+      <Shape bgColor='Green' type='circle' />
     </div>
   );
 }
---
+++ src/components/Shape.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Shape (props) {
+  const {bgColor = 'Blue', type = 'square'} = props;
+
+  const style = {
+    borderRadius: type === 'circle' ? '99999999px' : '0px',
+    width: '150px',
+    height: '150px',
+    backgroundColor: bgColor
+  }
+
+  return (
+    <div className='Shape' style={style} />
+  );
+}
+
+export default Shape;
