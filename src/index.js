4cc544ab0915428f07cc222f9490b75f0927b033
Steve Godin <steve.god@gmail.com>
2017-08-09 10:28:27 -0700

    Create project with 'Hello, World!'

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import registerServiceWorker from './registerServiceWorker';
+
+function Hello (props) {
+  return (
+    <h1>Hello, world!</h1>
+  );
+}
+
+ReactDOM.render(<Hello />, document.getElementById('root'));
+registerServiceWorker();
